50ff7e195f8cfe0cac237345fccc5883d22b4432
Hexilee <hexileee@gmail.com>
2018-01-27 05:20:25 +0800

    refactor config file load test with RunTest, seems all right

--- config_test.go
+++ config_test.go
@@ -12,45 +12,38 @@ type ConfigTestTypeJSON struct {
 	CONF `path:"./resources/application.yaml" type:"json"`
 }
 
-type ConfigTestSuffixYAML struct {
-	CONF `path:"./resources/application.yaml"`
-}
-
-type ConfigTestTypeYAML struct {
-	CONF `path:"./resources/application.yaml" type:"toml"`
-}
+//type ConfigTestSuffixYAML struct {
+//	CONF `path:"./resources/application.yaml"`
+//}
+//
+//type ConfigTestTypeYAML struct {
+//	CONF `path:"./resources/application.yaml" type:"toml"`
+//}
 
 var (
 	JSONConf, _ = GetJSONFromAnyFile(DefaultPath, JSON)
-	//JSONFromYAML = "{\"rhapsody\":{\"mysql\":{\"host\":\"localhost\"},\"redis\":{\"host\":\"127.0.0.1\",\"port\":6937}}}"
 	YAMLConf, _ = GetJSONFromAnyFile("./resources/application.yaml", JSON)
 )
 
-func TestConfigDefault(t *testing.T)  {
-	app := CreateApplication(new(ConfigTestDefault))
-	go app.Run()
-	t.Logf("conf:\n %s", app.ConfigFile)
-	assert.Equal(t, JSONConf, app.ConfigFile)
+type ConfigDefaultTest struct {
+	Testing
+	App *Application
 }
 
-func TestConfigTypeJSON(t *testing.T)  {
-	app := CreateApplication(new(ConfigTestTypeJSON))
-	go app.Run()
-	t.Logf("conf:\n %s", app.ConfigFile)
-	assert.Equal(t, YAMLConf, app.ConfigFile)
+func (c *ConfigDefaultTest) TestConfigDefault(t *testing.T) {
+	assert.Equal(t, JSONConf, c.App.ConfigFile)
 }
 
-//func TestConfigSuffixYAML(t *testing.T)  {
-//	app := CreateApplication(new(ConfigTestSuffixYAML))
-//	go app.Run()
-//	t.Logf("conf:\n %s", app.ConfigFile)
-//	assert.Equal(t, JSONFromYAML, app.ConfigFile)
-//}
-//
-//func TestConfigTypeYAML(t *testing.T)  {
-//	app := CreateApplication(new(ConfigTestTypeYAML))
-//	app.Run()
-//	t.Logf("conf:\n %s", app.ConfigFile)
-//	assert.Equal(t, JSONFromYAML, app.ConfigFile)
-//}
+type ConfigTypeJSONTest struct {
+	Testing
+	App *Application
+}
 
+func (c *ConfigTypeJSONTest) TestConfigTypeJSON(t *testing.T) {
+	assert.Equal(t, YAMLConf, c.App.ConfigFile)
+}
+
+func TestConfigFileLoad(t *testing.T) {
+	CreateApplication(new(ConfigTestDefault)).RunTest(t, new(ConfigDefaultTest))
+	CreateApplication(new(ConfigTestTypeJSON)).RunTest(t, new(ConfigTypeJSONTest))
+}
