87aa294df9dbe553476f8925c649719240f63cbf
Hexilee <hexileee@gmail.com>
2018-01-28 11:32:48 +0800

    move test_orm to rorm/test, ready to add Component DataBase

--- rorm/test/OrmTest.go
+++ rorm/test/OrmTest.go
@@ -9,11 +9,11 @@ import (
 
 type OrmTest struct {
 	rady.Testing
-	Repo *sqlite3.GormSQLiteRepo
+	SQLiteRepo *sqlite3.GormSQLiteRepo
 }
 
-func (orm *OrmTest) TestCRUD(t *testing.T) {
-	DB := orm.Repo.Begin()
+func (orm *OrmTest) TestSQLiteRepo(t *testing.T) {
+	DB := orm.SQLiteRepo.Begin()
 	DB.Create(&User{Name:"xixi", Age: 18})
 	NewUser := new(User)
 	DB.Where(&User{Name:"xixi"}).Find(&NewUser)
