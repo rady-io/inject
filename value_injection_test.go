ae70a66c9370a20ba0989e86878dc89baefe14ba
Hexilee <hexileee@gmail.com>
2018-02-05 02:39:10 +0800

    complete map value injection test, ready to test array

--- value_injection_test.go
+++ value_injection_test.go
@@ -4,9 +4,10 @@ import (
 	"testing"
 	"github.com/stretchr/testify/assert"
 	"time"
+	"github.com/tidwall/gjson"
 )
 
-const(
+const (
 	FormatTime = "2006-01-02 15:04:05"
 )
 
@@ -21,18 +22,19 @@ type (
 
 	ValueInjectTest struct {
 		Testing
-		RedisPortInt      int64      `value:"rady.redis.port"`
-		RedisPortIntPtr   *int64     `value:"rady.redis.port"`
-		RedisPortUint     uint64     `value:"rady.redis.port"`
-		RedisPortUintPtr  *uint64    `value:"rady.redis.port"`
-		RedisPortFloat    float64    `value:"rady.redis.port"`
-		RedisPortFloatPtr *float64   `value:"rady.redis.port"`
-		RedisPortStr      string     `value:"rady.redis.port"`
-		RedisPortStrPtr   *string    `value:"rady.redis.port"`
-		MysqlUtf8         bool       `value:"rady.mysql.utf-8"`
-		MysqlUtf8Ptr      *bool      `value:"rady.mysql.utf-8"`
-		JWTStartTime      time.Time  `value:"rady.jwt.start"`
-		JWTStartTimePtr   *time.Time `value:"rady.jwt.start"`
+		RedisPortInt      int64                   `value:"rady.redis.port"`
+		RedisPortIntPtr   *int64                  `value:"rady.redis.port"`
+		RedisPortUint     uint64                  `value:"rady.redis.port"`
+		RedisPortUintPtr  *uint64                 `value:"rady.redis.port"`
+		RedisPortFloat    float64                 `value:"rady.redis.port"`
+		RedisPortFloatPtr *float64                `value:"rady.redis.port"`
+		RedisPortStr      string                  `value:"rady.redis.port"`
+		RedisPortStrPtr   *string                 `value:"rady.redis.port"`
+		MysqlUtf8         bool                    `value:"rady.mysql.utf-8"`
+		MysqlUtf8Ptr      *bool                   `value:"rady.mysql.utf-8"`
+		JWTStartTime      time.Time               `value:"rady.jwt.start"`
+		JWTStartTimePtr   *time.Time              `value:"rady.jwt.start"`
+		RadyConfig        map[string]gjson.Result `value:"rady"`
 	}
 )
 
@@ -49,6 +51,37 @@ func (v *ValueInjectTest) TestElemOrPtrInject(t *testing.T) {
 	assert.True(t, *v.MysqlUtf8Ptr)
 	assert.Equal(t, TrueTime, v.JWTStartTime)
 	assert.Equal(t, TrueTime, *v.JWTStartTimePtr)
+}
+
+func (v *ValueInjectTest) TestMapInject(t *testing.T) {
+	for Key, Value := range v.RadyConfig {
+		switch Key {
+		case "mysql":
+			for key, value := range Value.Map() {
+				switch key {
+				case "host":
+					assert.Equal(t, "localhost", value.String())
+				case "utf-8":
+					assert.True(t, value.Bool())
+				default:
+				}
+			}
+		case "redis":
+			for key, value := range Value.Map() {
+				switch key {
+				case "host":
+					assert.Equal(t, "127.0.0.1", value.String())
+				case "utf-8":
+					assert.Equal(t, int64(6937), value.Int())
+				default:
+				}
+			}
+		case "jwt":
+			assert.Equal(t, TrueTime, Value.Time())
+		default:
+
+		}
+	}
 }
 
 func TestValueInjection(t *testing.T) {
